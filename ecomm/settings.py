82eb85b29fe26f5b68b560b76889cb77d6b5940a
shakirustad <ustadhismailgunia@gmail.com>
2025-02-08 18:28:34 +0300

    Ready for deployment

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -131,6 +131,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Add this line
     'allauth.account.middleware.AccountMiddleware',
 ]
 
@@ -204,6 +205,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "public/media")]
 
 # Media files
